9a9bb50c9ca76f5d49177d4c426147c6c725bcbd
speris <speris@grupokh.com>
2023-07-25 16:12:33 +0200

    Added Singleton pattern.

---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Call the GetInstance static method to get the Singleton Instance
+            Singleton fromMe = Singleton.GetInstance();
+            fromMe.PrinDetails("Hello from me");
+            
+            Singleton fromMyNeighbour = Singleton.GetInstance();
+            fromMyNeighbour.PrinDetails("Hello from my neighbour");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,37 @@
+﻿namespace Singleton
+{
+    //This class is sealed because I want only one instance
+    //Basically, seal the class, create a private constructor, and public static method to return the class.
+    public sealed class Singleton
+    {
+        //This variable value will be increment by 1 each time the object of the class is created
+        private static int Counter = 0;
+
+        //This variable is going to store the Singleton Instance
+        private static Singleton Instance = null;
+
+        public static Singleton GetInstance()
+        {
+            //If the variable instance is null, then create the Singleton instance 
+            //else return the already created singleton instance
+            //This version is not thread safe
+            if (Instance == null)
+            {
+                Instance = new Singleton();
+            }
+            //Return the Singleton Instance
+            return Instance;
+        }
+
+        private Singleton()
+        {
+            Counter++;
+            Console.WriteLine($"Counter: {Counter.ToString()}");
+        }
+
+        public void PrinDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}

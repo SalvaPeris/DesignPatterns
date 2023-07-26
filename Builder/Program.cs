9e3157cf4029f520a6fe55190295d07afabb7aca
speris <speris@grupokh.com>
2023-07-26 12:26:27 +0200

    Added Builder pattern.

---
+++ Builder/Program.cs
@@ -0,0 +1,23 @@
+﻿using Builder;
+using Builder.Reports;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Builder pattern example");
+        ReportDirector director = new ReportDirector();
+
+        Console.WriteLine("\n:::: PDF REPORT ::::");
+        PDFReport pdfReport = new PDFReport();
+        Report report = director.MakeReport(pdfReport);
+        report.DisplayReport();
+
+        Console.WriteLine("\n:::: EXCEL REPORT ::::");
+        ExcelReport excelReport = new ExcelReport();
+        report = director.MakeReport(excelReport);
+        report.DisplayReport();
+
+        Console.ReadKey();
+    }
+}
---
+++ Builder/Reports/ExcelReport.cs
@@ -0,0 +1,25 @@
+﻿namespace Builder.Reports
+{
+    public class ExcelReport : ReportBuilder
+    {
+        public override void SetReportContent()
+        {
+            reportObject.ReportContent = "Excel content";
+        }
+
+        public override void SetReportFooter()
+        {
+            reportObject.ReportFooter = "Excel footer";
+        }
+
+        public override void SetReportHeader()
+        {
+            reportObject.ReportHeader = "Excel header";
+        }
+
+        public override void SetReportType()
+        {
+            reportObject.ReportType = "Excel";
+        }
+    }
+}

3ad7c3f79fa00d21af7701d741e83f85f97e33a9
speris <speris@grupokh.com>
2023-08-02 09:36:17 +0200

    Added Fluent Interface finished.

---
+++ FluentInterface/FluentEmployee.cs
@@ -0,0 +1,38 @@
+﻿using FluentInterface.Models;
+
+namespace FluentInterface
+{
+    public class FluentEmployee
+    {
+        private Employee employee = new Employee();
+
+        public FluentEmployee NameOfTheEmployee(string fullName)
+        {
+            employee.FullName = fullName;
+            return this;
+        }
+    
+        public FluentEmployee Born(string dateOfBirth)
+        {
+            employee.DateOfBirth = Convert.ToDateTime(dateOfBirth); 
+            return this;
+        }
+
+        public FluentEmployee WorkingOn(string Department)
+        {
+            employee.Department = Department;
+            return this;
+        }
+
+        public FluentEmployee StaysAt(string Address)
+        {
+            employee.Address = Address;
+            return this;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine($"Name: {employee.FullName}, \nDateOfBirth: {employee.DateOfBirth}, \nDepartment: {employee.Department}, \nAddress: {employee.Address}");
+        }
+    }
+}
--- FluentInterface/Program.cs
+++ FluentInterface/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FluentInterface;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is a Fluent interface pattern demo");
+
+        FluentEmployee fluentEmployee = new FluentEmployee();
+
+        fluentEmployee.NameOfTheEmployee("Salva Peris")
+            .Born("03/11/1900")
+            .WorkingOn("Technology")
+            .StaysAt("Spain");
+
+        fluentEmployee.ShowDetails();
+        Console.ReadKey();
+    }
+}

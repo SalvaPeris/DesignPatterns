cbfba7d85eb6bbb611b54111c9c4e6795d910447
speris <speris@grupokh.com>
2023-09-01 08:41:55 +0200

    Flyweight pattern finished.

--- Flyweight/Program.cs
+++ Flyweight/Program.cs
@@ -1,7 +1,36 @@
-﻿internal class Program
+﻿using Flyweight;
+using Flyweight.Models;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("This is Flyweight pattern demo:");
+
+        Console.WriteLine("\n Red color circles ");
+        for (int i = 0; i < 3; i++)
+        {
+            Circle? circle = (Circle?)ShapeFactory.GetShape("circle");
+            circle?.SetColor("red");
+            circle?.Draw();
+        }
+
+        Console.WriteLine("\n Green color circles ");
+        for (int i = 0; i < 3; i++)
+        {
+            Circle? circle = (Circle?)ShapeFactory.GetShape("circle");
+            circle?.SetColor("green");
+            circle?.Draw();
+        }
+
+        Console.WriteLine("\n Black color circles ");
+        for (int i = 0; i < 3; i++)
+        {
+            Circle? circle = (Circle?)ShapeFactory.GetShape("circle");
+            circle?.SetColor("black");
+            circle?.Draw();
+        }
+
+        Console.ReadKey();
     }
 }
---
+++ Flyweight/ShapeFactory.cs
@@ -0,0 +1,30 @@
+﻿using Flyweight.Interfaces;
+using Flyweight.Models;
+
+namespace Flyweight
+{
+    public class ShapeFactory
+    {
+        private static Dictionary<string, IShape> shapeMap = new();
+
+        public static IShape? GetShape(string shapeName)
+        {
+            IShape? shape = null;
+
+            if(shapeName.Equals("circle", StringComparison.InvariantCultureIgnoreCase))
+            {
+                if(shapeMap.TryGetValue("circle", out shape))
+                {
+                }
+                else
+                {
+                    shape = new Circle();
+                    shapeMap.Add("circle", shape);
+                    Console.WriteLine("Creating circle object with out any color in shapefactory \n");
+                }
+            }
+
+            return shape;
+        }
+    }
+}

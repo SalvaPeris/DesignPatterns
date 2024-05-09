e0d90807701c9468fc1534700f0b4ef786c3cfca
SalvaPeris <sperisgimeno@gmail.com>
2024-05-09 11:16:34 +0200

    Added template class.

---
+++ Template/HouseTemplate.cs
@@ -0,0 +1,19 @@
+﻿namespace Template
+{
+    public abstract class HouseTemplate
+    {
+        public void BuildHouse()
+        {
+            BuildFoundation();
+            BuildPillars();
+            BuildWalls();
+            BuildWindows();
+            Console.WriteLine("House is Built");
+        }
+
+        protected abstract void BuildFoundation();
+        protected abstract void BuildPillars();
+        protected abstract void BuildWalls();
+        protected abstract void BuildWindows();
+    }
+}

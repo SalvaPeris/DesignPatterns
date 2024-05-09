40c9a64407b8974fb2f83df5b74b67ebe97d50e0
SalvaPeris <sperisgimeno@gmail.com>
2024-05-09 11:20:25 +0200

    Added the client in main method.

--- Template/Program.cs
+++ Template/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using Template;
+
+internal class Program
 {
     private static void Main()
     {
         Console.WriteLine("This is the template pattern demo:");
+        Console.WriteLine("\n \n");
+
+        Console.WriteLine("Building a Concrete house: ");
+        HouseTemplate houseTemplate = new ConcreteHouse();
+        houseTemplate.BuildHouse();
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Build a Wooden House:");
+        houseTemplate = new WoodenHouse();
+        houseTemplate.BuildHouse();
+        Console.Read();
     }
 }

29f971315774ff9be6f6fab314dc2b4df0919d22
SalvaPeris <sperisgimeno@gmail.com>
2024-05-10 14:05:23 +0200

    Added MenuOptions and completed Main.

---
+++ Command/MenuOptions.cs
@@ -0,0 +1,33 @@
+﻿using Command.Concrete;
+using Command.Interface;
+
+namespace Command
+{
+    public class MenuOptions
+    {
+        private ICommand _openCommand;
+        private ICommand _saveCommand;
+        private ICommand _closeCommand;
+        public MenuOptions(ICommand openCommand, ICommand saveCommand, ICommand closeCommand)
+        {
+            _openCommand = openCommand;
+            _saveCommand = saveCommand;
+            _closeCommand = closeCommand;
+        }
+
+        public void ClickOpen()
+        {
+            _openCommand.Execute();
+        }
+        
+        public void ClickSave()
+        {
+            _saveCommand.Execute();
+        }
+        
+        public void ClickClose()
+        {
+            _closeCommand.Execute();
+        }
+    }
+}
--- Command/Program.cs
+++ Command/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using Command.Concrete;
+using Command;
+using Command.Interface;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("This is the Command pattern demo:");
+        Console.WriteLine("This is the Command pattern demo: ");
+
+        Document document = new Document();
+
+        ICommand openCommand = new OpenCommand(document);
+        ICommand saveCommand = new SaveCommand(document);
+        ICommand closeCommand = new CloseCommand(document);
+        
+        MenuOptions menu = new MenuOptions(openCommand, saveCommand, closeCommand);
+        menu.ClickOpen();
+        menu.ClickSave();
+        menu.ClickClose();
+        
+        Console.ReadKey();
     }
 }

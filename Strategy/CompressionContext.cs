8880ae8fe9654aa3c170bb252a3df13bbf2efc43
SalvaPeris <sperisgimeno@gmail.com>
2024-05-14 15:30:17 +0200

    Finished Strategy pattern.

---
+++ Strategy/CompressionContext.cs
@@ -0,0 +1,24 @@
+﻿using Strategy.Interfaces;
+
+namespace Strategy
+{
+    public class CompressionContext
+    {
+        private ICompression Compression;
+
+        public CompressionContext(ICompression Compression)
+        {
+            this.Compression = Compression;
+        }
+
+        public void SetStrategy(ICompression Compression)
+        {
+            this.Compression = Compression;
+        }
+
+        public void CreateArchive(string compressedArchiveFileName)
+        {
+            Compression.CompressFolder(compressedArchiveFileName);
+        }
+    }
+}
--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using Strategy;
+using Strategy.Interfaces;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
         Console.WriteLine("This is a Strategy design pattern demo: \n");
+
+        ICompression strategy = new ZipCompression();
+
+        CompressionContext ctx = new CompressionContext(strategy);
+        ctx.CreateArchive("DotNetDesignPattern");
+
+        ctx.SetStrategy(new RarCompression());
+        ctx.CreateArchive("DotNetDesignPattern");
+
+        Console.Read();
     }
 }

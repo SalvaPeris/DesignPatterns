4a5a7eab14d1fbf2e7c6a9bb1214d96407696e6b
SalvaPeris <sperisgimeno@gmail.com>
2024-05-16 08:28:14 +0200

    Added interfaces.

---
+++ Mediator/Interfaces/IFacebookGroupMediator.cs
@@ -0,0 +1,8 @@
+﻿namespace Mediator.Interfaces
+{
+    public interface IFacebookGroupMediator
+    {
+        void SendMessage(string msg, User user);
+        void RegisterUser(User user);
+    }
+}

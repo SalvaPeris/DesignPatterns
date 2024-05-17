dbe5654d287a05c3775bbdc381506d2b1f6aa681
SalvaPeris <sperisgimeno@gmail.com>
2024-05-17 12:54:01 +0200

    Added memento class.

---
+++ Memento/Memento.cs
@@ -0,0 +1,17 @@
+﻿namespace Memento
+{
+    public class Memento
+    {
+        public LedTV LedTV { get; set; }
+        
+        public Memento(LedTV ledTV)
+        {
+            LedTV = ledTV;
+        }
+
+        public string GetDetails()
+        {
+            return "Memento [Led TV= " + LedTV.GetDetails() + "]";
+        }
+    }
+}

5e4278bc812a52c16af9a3d7ef7e945a6fd270a1
SalvaPeris <sperisgimeno@gmail.com>
2024-05-17 12:52:29 +0200

    Added LedTV class.

---
+++ Memento/LedTV.cs
@@ -0,0 +1,20 @@
+﻿namespace Memento
+{
+    public class LedTV
+    {
+        public string Size { get; set; }
+        public string Price { get; set; }
+        public bool USBSupport { get; set; }
+        
+        public LedTV(string Size, string Price, bool USBSupport)
+        {
+            this.Size = Size;
+            this.Price = Price;
+            this.USBSupport = USBSupport;
+        }
+        public string GetDetails()
+        {
+            return "Led TV [Size=" + Size + ", Price=" + Price + ", USBSupport=" + USBSupport + "]";
+        }
+    }
+}

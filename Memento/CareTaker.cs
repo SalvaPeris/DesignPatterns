8dbb0bec25f394f3a98c03cccad548c7133885e8
SalvaPeris <sperisgimeno@gmail.com>
2024-05-17 12:56:10 +0200

    Added Caretaker and Originator classes.

---
+++ Memento/CareTaker.cs
@@ -0,0 +1,18 @@
+﻿namespace Memento
+{
+    public class Caretaker
+    {
+        private List<Memento> LedTvList = [];
+        
+        public void AddMemento(Memento m)
+        {
+            LedTvList.Add(m);
+            Console.WriteLine("Led TV's snapshots Maintained by CareTaker :" + m.GetDetails());
+        }
+        
+        public Memento GetMemento(int index)
+        {
+            return LedTvList[index];
+        }
+    }
+}
---
+++ Memento/Originator.cs
@@ -0,0 +1,22 @@
+﻿namespace Memento
+{
+    public class Originator
+    {
+        public LedTV? LedTV;
+        
+        public Memento CreateMemento()
+        {
+            return new Memento(LedTV);
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            LedTV = memento.LedTV;
+        }
+        
+        public string GetDetails()
+        {
+            return "Originator [Led TV=" + LedTV.GetDetails() + "]";
+        }
+    }
+}
